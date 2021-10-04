c1575b3bd616c9ab99359944209770b728cd1ac7
Kth <kth.ortegon@gmail.com>
2021-10-03 20:32:46 -0500

    Spring boot en blanco

---
+++ src/test/java/TiendaWeb55_GroupG/TiendaWeb/TiendaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaWeb55_GroupG.TiendaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
